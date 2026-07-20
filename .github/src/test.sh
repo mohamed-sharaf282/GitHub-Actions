8cd2ee014f64e55603f900fd3be99117f3c240b7
mohamed-sharaf282 <mohamedsharf0000@gmail.com>
2026-07-20 19:55:49 +0300

    second workflow

--- .github/src/test.sh
+++ .github/src/test.sh
@@ -4,7 +4,7 @@
 
 EXPECTED="Hello, Test!"
 
-OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
+OUTPUT=$(node -e "console.log(require('./src/app')('Test'))") #000
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
     echo "✅ Test passed!"
